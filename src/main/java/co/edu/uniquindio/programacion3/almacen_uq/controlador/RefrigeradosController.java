fed11ff47af3ebd4e91e99d24abf525ae9a87b19
RamiroRojasBociga <ramiro.rojasb@uqvirtual.edu.co>
2023-08-18 11:37:09 -0500

    Se termina refrigerados-view y se trabaja en la clase RefrigeradosController

--- src/main/java/co/edu/uniquindio/programacion3/almacen_uq/controlador/RefrigeradosController.java
+++ src/main/java/co/edu/uniquindio/programacion3/almacen_uq/controlador/RefrigeradosController.java
@@ -1,4 +1,45 @@
 package co.edu.uniquindio.programacion3.almacen_uq.controlador;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.layout.AnchorPane;
+
 public class RefrigeradosController {
+
+    @FXML
+    private Button btnActualizar;
+
+    @FXML
+    private Button btnAtras;
+
+    @FXML
+    private AnchorPane btnCrear;
+
+    @FXML
+    private Button btnEliminar;
+
+    @FXML
+    private Button btnLimpiar;
+
+    @FXML
+    void actualizarRefrigerado(ActionEvent event) {
+
+    }
+
+    @FXML
+    void eliminarRefrigerado(ActionEvent event) {
+
+    }
+
+    @FXML
+    void limpiarCampo(ActionEvent event) {
+
+    }
+
+    @FXML
+    void menuInicioProductos(ActionEvent event) {
+
+    }
+
 }
