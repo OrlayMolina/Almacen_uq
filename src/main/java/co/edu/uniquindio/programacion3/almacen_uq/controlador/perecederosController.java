9180cc9c58c39593940221db1a0f4ae711cb56e7
Yarley Camilo Mejia <100544029+YarleyMejia@users.noreply.github.com>
2023-08-19 02:45:18 -0500

    VistaPerecederos
    Se sube vista de perecederos.

---
+++ src/main/java/co/edu/uniquindio/programacion3/almacen_uq/controlador/perecederosController.java
@@ -0,0 +1,103 @@
+package co.edu.uniquindio.programacion3.almacen_uq.controlador;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.DatePicker;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafx.scene.control.TextArea;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.GridPane;
+
+public class clientesController {
+
+    @FXML
+    private TableColumn<?, ?> colExistencias;
+
+    @FXML
+    private TableColumn<?, ?> colCodigo;
+
+    @FXML
+    private TableColumn<?, ?> colFechaVencimiento;
+
+    @FXML
+    private Button btnLimpiarPerecedero;
+
+    @FXML
+    private TableView<?> tablePerecederos;
+
+    @FXML
+    private TableColumn<?, ?> colNombre;
+
+    @FXML
+    private TextField txtCodigo;
+
+    @FXML
+    private GridPane txtValorUnitario;
+
+    @FXML
+    private DatePicker dateFechaVencimiento;
+
+    @FXML
+    private TextArea txtDEscripcion;
+
+    @FXML
+    private TextField txtNombre;
+
+    @FXML
+    private TableColumn<?, ?> colDescripcion;
+
+    @FXML
+    private TextField txtExistencias;
+
+    @FXML
+    private Button btnCrearPerecedero;
+
+    @FXML
+    private Button btnEliminarPerecedero;
+
+    @FXML
+    private TableColumn<?, ?> colValor;
+
+    @FXML
+    private Button btnSalirPrincipal;
+
+    @FXML
+    private Button btnActualizarPerecedero;
+
+    @FXML
+    void ebebeb(ActionEvent event) {
+
+    }
+
+    @FXML
+    void ebebeb(ActionEvent event) {
+
+    }
+
+    @FXML
+    void crearPerecedero(ActionEvent event) {
+
+    }
+
+    @FXML
+    void actualizarPerecedero(ActionEvent event) {
+
+    }
+
+    @FXML
+    void eliminarPerecedero(ActionEvent event) {
+
+    }
+
+    @FXML
+    void limpiarCampoPerecedero(ActionEvent event) {
+
+    }
+
+    @FXML
+    void btnSalirPrincipal(ActionEvent event) {
+
+    }
+
+}
