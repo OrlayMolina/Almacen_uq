2bac47402cbd7e84857739c35de916d36b57ba42
OrlayMolina <111409267+OrlayMolina@users.noreply.github.com>
2023-08-21 14:51:20 -0500

    Se reforma la clase de perecederosController para que pueda inicializar correctamente los datos en la tabla de perecederos, al igual que los metodos de crear, eliminar y actualizar perecederos funciones correctamente con sus respectivos mensajes de informacion al usuario.

--- src/main/java/co/edu/uniquindio/programacion3/almacen_uq/controlador/PerecederosController.java
+++ src/main/java/co/edu/uniquindio/programacion3/almacen_uq/controlador/PerecederosController.java
@@ -1,33 +1,37 @@
 package co.edu.uniquindio.programacion3.almacen_uq.controlador;
 
+import co.edu.uniquindio.programacion3.almacen_uq.enumm.Pais;
+import co.edu.uniquindio.programacion3.almacen_uq.factory.Factory;
 import co.edu.uniquindio.programacion3.almacen_uq.main.App;
 import co.edu.uniquindio.programacion3.almacen_uq.modelo.ProductoEnvasado;
 import co.edu.uniquindio.programacion3.almacen_uq.modelo.ProductoPerecedero;
 import co.edu.uniquindio.programacion3.almacen_uq.modelo.ProductoRefrigerado;
 import co.edu.uniquindio.programacion3.almacen_uq.persistencia.Persistencia;
+import co.edu.uniquindio.programacion3.almacen_uq.subcontrolador.EnvasadoSubController;
+import co.edu.uniquindio.programacion3.almacen_uq.subcontrolador.PerecederoSubController;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
-import javafx.scene.control.Button;
-import javafx.scene.control.DatePicker;
-import javafx.scene.control.TableColumn;
-import javafx.scene.control.TableView;
-import javafx.scene.control.TextArea;
-import javafx.scene.control.TextField;
+import javafx.fxml.Initializable;
+import javafx.scene.control.*;
+import javafx.scene.control.cell.PropertyValueFactory;
 import javafx.scene.layout.GridPane;
 import javafx.event.ActionEvent;
 import javafx.stage.Stage;
 
+import java.net.URL;
 import java.time.LocalDate;
+import java.util.Optional;
+import java.util.ResourceBundle;
 
-public class PerecederosController {
+public class PerecederosController implements Initializable {
 
     App app = new App();
 
-    private final ObservableList<ProductoEnvasado> listaProductosRefrigerado = FXCollections.observableArrayList();
+    private final ObservableList<ProductoPerecedero> listaProductosPerecederos = FXCollections.observableArrayList();
 
-    private ProductoRefrigerado productoRefrigerado;
-    private ProductoRefrigerado subController;
+    private ProductoPerecedero productoPerecedero;
+    private PerecederoSubController perecederoSubController;
     private Persistencia persistencia = new Persistencia();
 
     @FXML
@@ -43,7 +47,7 @@ public class PerecederosController {
     private Button btnLimpiarPerecedero;
 
     @FXML
-    private TableView<ProductoRefrigerado> tablePerecederos;
+    private TableView<ProductoPerecedero> tablePerecederos;
 
     @FXML
     private TableColumn<ProductoPerecedero, String> colNombre;
@@ -52,19 +56,19 @@ public class PerecederosController {
     private TextField txtCodigo;
 
     @FXML
-    private GridPane txtValorUnitario;
+    private TextField txtValorUnitario;
 
     @FXML
     private DatePicker dateFechaVencimiento;
 
     @FXML
-    private TextArea txtDEscripcion;
+    private TextArea txtDescripcion;
 
     @FXML
     private TextField txtNombre;
 
     @FXML
-    private TableColumn<?, ?> colDescripcion;
+    private TableColumn<ProductoPerecedero, String> colDescripcion;
 
     @FXML
     private TextField txtExistencias;
@@ -76,34 +80,13 @@ public class PerecederosController {
     private Button btnEliminarPerecedero;
 
     @FXML
-    private TableColumn<?, ?> colValor;
+    private TableColumn<ProductoPerecedero, Double> colValor;
 
     @FXML
     private Button btnSalirPrincipal;
 
     @FXML
     private Button btnActualizarPerecedero;
-
-
-    @FXML
-    void crearPerecedero(ActionEvent event) {
-
-    }
-
-    @FXML
-    void actualizarPerecedero(ActionEvent event) {
-
-    }
-
-    @FXML
-    void eliminarPerecedero(ActionEvent event) {
-
-    }
-
-    @FXML
-    void limpiarCampoPerecedero(ActionEvent event) {
-
-    }
 
     @FXML
     void btnSalirPrincipal(ActionEvent event) {
@@ -112,13 +95,193 @@ public class PerecederosController {
     }
 
     @FXML
-    void menuInicioProductos(ActionEvent event) {
-        cerrarVentana(btnSalirPrincipal);
-        app.cargarVentanaProductosInicio();
+    void crearPerecedero(ActionEvent event) {
+        guardarNuevoPerecedero();
+    }
+
+    @FXML
+    void actualizarPerecedero(ActionEvent event) {
+        actualizarPerecedero();
+    }
+
+    @FXML
+    void eliminarPerecedero(ActionEvent event) {
+        eliminarPerecedero();
+    }
+
+    @FXML
+    void limpiarCampoPerecedero(ActionEvent event) {
+        txtCodigo.setText(null);
+        txtNombre.setText(null);
+        txtDescripcion.setText(null);
+        txtValorUnitario.setText(null);
+        txtExistencias.setText(null);
+        dateFechaVencimiento.setValue(null);
 
     }
+
+    public void guardarNuevoPerecedero(){
+        ProductoPerecedero perecedero = new ProductoPerecedero();
+
+        try {
+            String codigo = txtCodigo.getText();
+            String nombreProducto = txtNombre.getText();
+            String descripcion = txtDescripcion.getText();
+            Double valorUnitario = Double.valueOf(txtValorUnitario.getText());
+            int existencias = Integer.parseInt(txtExistencias.getText());
+            LocalDate fechaEvasado = dateFechaVencimiento.getValue();
+
+            perecedero.setCodigo(codigo);
+            perecedero.setNombreProducto(nombreProducto);
+            perecedero.setDescripcion(descripcion);
+            perecedero.setValorUnitario(valorUnitario);
+            perecedero.setExistencias(existencias);
+            perecedero.setFechaVencimiento(fechaEvasado);
+
+            perecederoSubController.crearProductoPerecedero(perecedero);
+            listaProductosPerecederos.add(perecedero);
+            tablePerecederos.refresh();
+            mostrarMensaje("CREACIÓN", "Creación de Producto Perecedero",
+                    "El Producto Perecedero se ha creado correctamente", Alert.AlertType.INFORMATION);
+        }catch (Exception e){
+            mostrarMensaje("CREACIÓN","Creación de Producto Perecedero.",
+                    "El Producto Perecedero no se pudo actualizar.", Alert.AlertType.WARNING);
+        }
+
+    }
+
+    public void eliminarPerecedero() {
+        boolean bandera = false, mensaje;
+        if (productoPerecedero != null) {
+            mensaje = mostrarMensajeConfirmacion("¿Está seguro que desea eliminar el producto?.");
+            if (mensaje) {
+                bandera = perecederoSubController.eliminarPerecedero(productoPerecedero);
+                if (bandera) {
+                    listaProductosPerecederos.remove(productoPerecedero);
+                    productoPerecedero = null;
+                    tablePerecederos.getSelectionModel().clearSelection();
+                    mostrarMensaje("ELIMINACIÓN", "Eliminación de Producto Perecedero",
+                            "El Producto Perecedero se ha eliminado correctamente", Alert.AlertType.INFORMATION);
+                } else {
+                    mostrarMensaje("ELIMINACIÓN", "Eliminación de Producto Perecedero.",
+                            "El Producto Perecedero no se pudo eliminar.", Alert.AlertType.WARNING);
+                }
+            }
+        }
+    }
+
+    public void actualizarPerecedero(){
+        ProductoPerecedero perecedero = new ProductoPerecedero();
+        boolean bandera = false;
+
+        String codigo = txtCodigo.getText();
+        String nombreProducto = txtNombre.getText();
+        String descripcion = txtDescripcion.getText();
+        Double valorUnitario = Double.valueOf(txtValorUnitario.getText());
+        int existencias = Integer.parseInt(txtExistencias.getText());
+        LocalDate fechaVencimiento = dateFechaVencimiento.getValue();
+
+        perecedero.setCodigo(codigo);
+        perecedero.setNombreProducto(nombreProducto);
+        perecedero.setDescripcion(descripcion);
+        perecedero.setValorUnitario(valorUnitario);
+        perecedero.setExistencias(existencias);
+        perecedero.setFechaVencimiento(fechaVencimiento);
+
+        bandera = perecederoSubController.actualizarPerecedero(perecedero);
+
+        if(bandera){
+
+            tablePerecederos.refresh();
+            mostrarMensaje("ACTUALIZACIÓN","Actualización de Producto.",
+                    "El Producto se actualizó correctamente.", Alert.AlertType.INFORMATION);
+
+        }else {
+            mostrarMensaje("ACTUALIZACIÓN","Actualización de Producto.",
+                    "El Producto no se pudo actualizar.", Alert.AlertType.WARNING);
+        }
+
+    }
+
+    public ObservableList<ProductoPerecedero> getProductoPerecedero() {
+        listaProductosPerecederos.addAll(perecederoSubController.obtenerPerecedero());
+        return listaProductosPerecederos;
+    }
+
+    private void inicializarPerecederosView() {
+
+        colCodigo.setCellValueFactory(new PropertyValueFactory<>("codigo"));
+        colNombre.setCellValueFactory(new PropertyValueFactory<>("nombreProducto"));
+        colValor.setCellValueFactory(new PropertyValueFactory<>("valorUnitario"));
+        colFechaVencimiento.setCellValueFactory(new PropertyValueFactory<>("fechaVencimiento"));
+        colDescripcion.setCellValueFactory(new PropertyValueFactory<>("descripcion"));
+        colExistencias.setCellValueFactory(new PropertyValueFactory<>("existencias"));
+
+        tablePerecederos.getItems().clear();
+        tablePerecederos.setItems(getProductoPerecedero());
+
+        tablePerecederos.getSelectionModel().selectedItemProperty().addListener((obs, oldSelection, newSelection) -> {
+
+            productoPerecedero = (ProductoPerecedero) newSelection;
+
+            mostrarInformacionSerie(productoPerecedero);
+
+        });
+
+    }
+    private void mostrarInformacionSerie(ProductoPerecedero perecedero) {
+        if(perecedero != null) {
+
+            txtCodigo.setText(perecedero.getCodigo());
+            txtNombre.setText(perecedero.getNombreProducto());
+            txtDescripcion.setText(perecedero.getDescripcion());
+            txtValorUnitario.setText(String.valueOf(perecedero.getValorUnitario()));
+            txtExistencias.setText(String.valueOf(perecedero.getExistencias()));
+            dateFechaVencimiento.setValue(perecedero.getFechaVencimiento());
+
+        }
+    }
+
+    public boolean mostrarMensajeConfirmacion(String mensaje) {
+
+        Alert alert = new Alert(Alert.AlertType.CONFIRMATION);
+        alert.setHeaderText(null);
+        alert.setTitle("Confirmacion");
+        alert.setContentText(mensaje);
+        Optional<ButtonType> action = alert.showAndWait();
+
+        if (action.get() == ButtonType.OK) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    public void mostrarMensaje(String titulo, String header, String contenido, Alert.AlertType alertType) {
+
+        Alert aler = new Alert(alertType);
+        aler.setTitle(titulo);
+        aler.setHeaderText(header);
+        aler.setContentText(contenido);
+        aler.showAndWait();
+    }
+
+    public void datosInicialesPerecederos(){
+        Factory factory = Factory.getInstance();
+        perecederoSubController = new PerecederoSubController(factory);
+        new EnvasadosController();
+        inicializarPerecederosView();
+    }
+
+
     public void cerrarVentana(Button btn) {
         Stage stage = (Stage) btn.getScene().getWindow();
         stage.close();
+    }
+
+    @Override
+    @FXML
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+        datosInicialesPerecederos();
     }
 }
